bd14139ad0bb7e548652b191d9d11847953ea069
William <will.h.emory@gmail.com>
2018-12-02 12:22:52 -0600

    HEllo Database example

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ app.get('/', function (req, res) {
    res.send('Hello World from GCE!'); 
 });
 
-app.listen(3000, function () {
-   console.log('Example app listening on port 3000!');
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'Whemory',
+        password: 'Third1993$',
+        server: '35.225.192.81', 
+        database: 'The_Webapp' 
+    };
+
+    // connect to your database:
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT * FROM dbo.Employee', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(3000, function () {
+    console.log('Server is running..');
 });
